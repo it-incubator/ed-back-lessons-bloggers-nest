8ddc33e46f96cc47ea26b1d1d184256284ff8e37
itolkachev <vantrey@yandex.ru>
2025-01-16 12:14:57 +0300

    compare lesson 2 refactoring optional guard

--- src/features/user-accounts/guards/bearer/jwt-optional-auth.guard.ts
+++ src/features/user-accounts/guards/bearer/jwt-optional-auth.guard.ts
@@ -10,5 +10,27 @@ export class JwtOptionalAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  //handleRequest(err: any, user: any, info: any) {}
+  handleRequest(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ) {
+    //super.handleRequest(err, user, info, context, status);
+    // мы не будем вызывать здесь базовый метод суперкласса, в нём написано вот это:
+    // кидаем ошибку если нет юзера или если другая ошибка (например JWT протух)...
+    // handleRequest(err, user, info, context, status) {
+    //   if (err || !user) {
+    //     throw err || new common_1.UnauthorizedException();
+    //   }
+    //   return user;
+    // }
+    // а мы вернём просто null и не будем процессить ошибку и null
+    if (err || !user) {
+      return null;
+    } else {
+      return user;
+    }
+  }
 }
