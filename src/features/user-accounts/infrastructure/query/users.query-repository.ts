cad8752340d1484d54f9cefd9f73315e31b6d3f6
itolkachev <vantrey@yandex.ru>
2025-02-06 13:44:55 +0300

    remove deletion status

--- src/features/user-accounts/infrastructure/query/users.query-repository.ts
+++ src/features/user-accounts/infrastructure/query/users.query-repository.ts
@@ -1,4 +1,4 @@
-import { DeletionStatus, User, UserModelType } from '../../domain/user.entity';
+import { User, UserModelType } from '../../domain/user.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserViewDto } from '../../api/view-dto/users.view-dto';
 import { Injectable } from '@nestjs/common';
@@ -26,7 +26,7 @@ export class UsersQueryRepository
   async getByIdOrNotFoundFail(id: Types.ObjectId): Promise<UserViewDto> {
     const user = await this.UserModel.findOne({
       _id: id,
-      deletionStatus: DeletionStatus.NotDeleted,
+      deletedAt: null,
     });
 
     if (!user) {
@@ -40,7 +40,7 @@ export class UsersQueryRepository
     query: GetUsersQueryParams,
   ): Promise<PaginatedViewDto<UserViewDto[]>> {
     const filter: FilterQuery<User> = {
-      deletionStatus: DeletionStatus.NotDeleted,
+      deletedAt: null,
     };
 
     if (query.searchLoginTerm) {
