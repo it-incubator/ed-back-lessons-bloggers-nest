d1cc0bad01e54547169baedc457d120fb5c6de8f
itolkachev <vantrey@yandex.ru>
2025-03-19 14:53:49 +0300

    refactor pagenatedViewDto

--- src/core/dto/base.query-params.input-dto.ts
+++ src/core/dto/base.query-params.input-dto.ts
@@ -1,13 +1,14 @@
-//базовый класс для query параметров с пагинацией
-//значения по-умолчанию применятся автоматически при настройке глобального ValidationPipe в main.ts
 import { Type } from 'class-transformer';
 
-class PaginationParams {
+//базовый класс для query параметров с пагинацией
+//значения по-умолчанию применятся автоматически при настройке глобального ValidationPipe в main.ts
+export class BaseQueryParams {
   //для трансформации в number
   @Type(() => Number)
   pageNumber: number = 1;
   @Type(() => Number)
   pageSize: number = 10;
+  sortDirection: SortDirection = SortDirection.Desc;
 
   calculateSkip() {
     return (this.pageNumber - 1) * this.pageSize;
@@ -17,11 +18,4 @@ class PaginationParams {
 export enum SortDirection {
   Asc = 'asc',
   Desc = 'desc',
-}
-
-//базовый класс для query параметров с сортировкой и пагинацией
-//поле sortBy должно быть реализовано в наследниках
-export abstract class BaseSortablePaginationParams<T> extends PaginationParams {
-  sortDirection: SortDirection = SortDirection.Desc;
-  abstract sortBy: T;
 }
--- src/features/user-accounts/api/input-dto/get-users-query-params.input-dto.ts
+++ src/features/user-accounts/api/input-dto/get-users-query-params.input-dto.ts
@@ -1,8 +1,9 @@
 //dto для запроса списка юзеров с пагинацией, сортировкой, фильтрами
-import { BaseSortablePaginationParams } from '../../../../core/dto/base.query-params.input-dto';
 import { UsersSortBy } from './users-sort-by';
+import { BaseQueryParams } from '../../../../core/dto/base.query-params.input-dto';
 
-export class GetUsersQueryParams extends BaseSortablePaginationParams<UsersSortBy> {
+//наследуемся от класса BaseQueryParams, где уже есть pageNumber, pageSize и т.п., чтобы не дублировать эти свойства
+export class GetUsersQueryParams extends BaseQueryParams {
   sortBy = UsersSortBy.CreatedAt;
   searchLoginTerm: string | null = null;
   searchEmailTerm: string | null = null;
