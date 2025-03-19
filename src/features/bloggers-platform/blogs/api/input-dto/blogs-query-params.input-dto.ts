409f79b933e8b99bb19f96cb0583ccc3380e941f
itolkachev <vantrey@yandex.ru>
2025-03-19 16:07:12 +0300

    fix blogs query params

--- src/features/bloggers-platform/blogs/api/input-dto/blogs-query-params.input-dto.ts
+++ src/features/bloggers-platform/blogs/api/input-dto/blogs-query-params.input-dto.ts
@@ -1,8 +1,8 @@
 import { BlogsSortBy } from './blogs-sort-by';
 import { IsEnum } from 'class-validator';
-import { BaseSortablePaginationParams } from '../../../../../core/dto/base.query-params.input-dto';
+import { BaseQueryParams } from '../../../../../core/dto/base.query-params.input-dto';
 
-export class GetBlogsQueryParams extends BaseSortablePaginationParams<BlogsSortBy> {
+export class GetBlogsQueryParams extends BaseQueryParams {
   @IsEnum(BlogsSortBy)
   sortBy = BlogsSortBy.CreatedAt;
 }
