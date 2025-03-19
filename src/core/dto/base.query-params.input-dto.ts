eb485297afcfdefa93b8a5bd026cc8cd790b6272
itolkachev <vantrey@yandex.ru>
2025-03-19 14:53:49 +0300

    refactor pagenatedViewDto

--- src/core/dto/base.query-params.input-dto.ts
+++ src/core/dto/base.query-params.input-dto.ts
@@ -1,27 +1,25 @@
-//базовый класс для query параметров с пагинацией
-//значения по-умолчанию применятся автоматически при настройке глобального ValidationPipe в main.ts
 import { Type } from 'class-transformer';
-
-class PaginationParams {
-  //для трансформации в number
-  @Type(() => Number)
-  pageNumber: number = 1;
-  @Type(() => Number)
-  pageSize: number = 10;
-
-  calculateSkip() {
-    return (this.pageNumber - 1) * this.pageSize;
-  }
-}
+import { IsEnum, IsNumber } from 'class-validator';
 
 export enum SortDirection {
   Asc = 'asc',
   Desc = 'desc',
 }
 
-//базовый класс для query параметров с сортировкой и пагинацией
-//поле sortBy должно быть реализовано в наследниках
-export abstract class BaseSortablePaginationParams<T> extends PaginationParams {
+//базовый класс для query параметров с пагинацией
+//значения по-умолчанию применятся автоматически при настройке глобального ValidationPipe в main.ts
+export class BaseQueryParams {
+  //для трансформации в number
+  @Type(() => Number)
+  @IsNumber()
+  pageNumber: number = 1;
+  @Type(() => Number)
+  @IsNumber()
+  pageSize: number = 10;
+  @IsEnum(SortDirection)
   sortDirection: SortDirection = SortDirection.Desc;
-  abstract sortBy: T;
+
+  calculateSkip() {
+    return (this.pageNumber - 1) * this.pageSize;
+  }
 }
