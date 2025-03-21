091f08f2ae805108e3af62fbdfeb01840c61baed
itolkachev <vantrey@yandex.ru>
2025-03-21 17:14:37 +0300

    simplify AllHttpExceptionFilter

--- src/core/exceptions/filters/all-exceptions.filter.ts
+++ src/core/exceptions/filters/all-exceptions.filter.ts
@@ -2,7 +2,6 @@ import {
   ArgumentsHost,
   Catch,
   ExceptionFilter,
-  HttpException,
   HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
@@ -14,40 +13,27 @@ import { CoreConfig } from '../../core.config';
 //Все ошибки
 @Catch()
 export class AllHttpExceptionsFilter implements ExceptionFilter {
-  constructor(private coreConfig: CoreConfig) {
-    console.log(coreConfig);
-  }
+  constructor(private coreConfig: CoreConfig) {}
 
-  catch(exception: unknown, host: ArgumentsHost): void {
+  catch(exception: any, host: ArgumentsHost): void {
+    //ctx нужен, чтобы получить request и response (express). Это из документации, делаем по аналогии
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    const status = this.resolveHttpStatus(exception);
-    const message = this.extractMessage(exception);
-    const responseBody = this.buildResponseBody(status, request.url, message);
+    //Если сработал этот фильтр, то пользователю улетит 500я ошибка
+    const message = exception.message || 'Unknown exception occurred.';
+    const status = HttpStatus.INTERNAL_SERVER_ERROR;
+    const responseBody = this.buildResponseBody(request.url, message);
 
     response.status(status).json(responseBody);
   }
 
-  private resolveHttpStatus(exception: unknown): number {
-    //хотя мы используем кастомные domain exception,
-    //NestJs все еще может выбросить стандартное http исключение в определенных сценариях
-    //например, если мы не переопределили поведение passport strategy
-    return exception instanceof HttpException
-      ? exception.getStatus()
-      : HttpStatus.INTERNAL_SERVER_ERROR;
-  }
-
   private buildResponseBody(
-    status: number,
     requestUrl: string,
     message: string,
   ): ErrorResponseBody {
-    if (
-      !this.coreConfig.sendInternalServerErrorDetails &&
-      status === HttpStatus.INTERNAL_SERVER_ERROR
-    ) {
+    if (!this.coreConfig.sendInternalServerErrorDetails) {
       return {
         timestamp: new Date().toISOString(),
         path: null,
@@ -64,13 +50,5 @@ export class AllHttpExceptionsFilter implements ExceptionFilter {
       extensions: [],
       code: DomainExceptionCode.InternalServerError,
     };
-  }
-
-  private extractMessage(exception: unknown): string {
-    const UNKNOWN_ERROR_MESSAGE = 'Internal server unknown error';
-    if (exception instanceof Error) {
-      return exception.message || UNKNOWN_ERROR_MESSAGE;
-    }
-    return UNKNOWN_ERROR_MESSAGE;
   }
 }
