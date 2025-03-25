34deec48ec521f3d594453a98ec8b8d13b24e2a0
itolkachev <vantrey@yandex.ru>
2025-03-25 16:29:34 +0300

    replace features folder with modules

--- src/modules/user-accounts/application/users.service.ts
+++ src/modules/user-accounts/application/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserModelType } from '../domain/user.entity';
 import { CreateUserDto, UpdateUserDto } from '../dto/create-user.dto';
